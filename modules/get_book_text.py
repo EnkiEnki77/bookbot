82bdc1ba5cada134882261499b7e6118315178b8
remdu <layman212@gmail.com>
2025-04-01 06:53:52 -0400

    accumulated functions into single stats module

--- modules/get_book_text.py
+++
@@ -1,9 +0,0 @@
-
-
-def get_book_text(filepath:str) -> str:
-    """Reads from file at filepath returning the content of the file"""
-
-    with open(filepath, 'r', encoding='utf-8') as f:
-        file_content = f.read()
-
-    return file_content
--- modules/stats.py
+++ modules/stats.py
@@ -1,4 +1,9 @@
+def get_book_text(filepath:str) -> str:
 
+    with open(filepath) as f:
+        file_content = f.read()
+
+    return file_content
 
 def get_book_word_count(book_content:str) -> int:
     """Takes a books content as input and returns the number of words in that content"""
