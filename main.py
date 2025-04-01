bbdc57b350eea409c196788934df260fe62941c8
remdu <layman212@gmail.com>
2025-04-01 06:48:42 -0400

    get_book_word_count works

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from modules.get_book_text import get_book_text
+from modules.get_book_word_count import get_book_word_count
+
+
 def main():
-        print("Hello world!")
+        print(get_book_word_count(get_book_text("./books/frankenstein.txt")))
 
 
 if __name__ == "__main__":
